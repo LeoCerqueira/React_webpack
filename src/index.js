e13d03e95f9366437bdf8f77e22c22d31f51f547
LeoCerqueira <ll_cerqueira@hotmail.com>
2021-07-07 09:35:55 -0300

    soucemap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import { render } from './react-dom';
+import ReactDOM from 'react-dom';
 import Title from './app';
 
-render(<Title />, document.querySelector('[data-js="app"]'));
+ReactDOM.render(<Title />, document.querySelector('[data-js="app"]'));
